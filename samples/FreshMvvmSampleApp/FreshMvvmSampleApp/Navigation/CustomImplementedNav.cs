c31b11a9ddeff0c49c740965a53ec2f8b92f4fa6
Gerald Versluis <gerald@verslu.is>
2015-07-09 16:00:16 +0200

    Fixed CustomImplementedNav (wasn't implementing new interface PopToRoot method) and exposed private properties in FreshMasterDetailNavigationContainer so that it's better inheritable

--- samples/FreshMvvmSampleApp/FreshMvvmSampleApp/Navigation/CustomImplementedNav.cs
+++ samples/FreshMvvmSampleApp/FreshMvvmSampleApp/Navigation/CustomImplementedNav.cs
@@ -1,88 +1,93 @@
-﻿using System;
-using FreshMvvm;
-using Xamarin.Forms;
-using System.Collections.Generic;
+﻿using FreshMvvm;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace FreshMvvmSampleApp
 {
-	/// <summary>
-	/// This is a sample custom implemented Navigation. It combines a MasterDetail and a TabbedPage.
-	/// </summary>
-	public class CustomImplementedNav : Xamarin.Forms.MasterDetailPage, IFreshNavigationService
-	{
-		FreshTabbedNavigationContainer _tabbedNavigationPage;
-		Page _contactsPage, _quotesPage;
+    /// <summary>
+    /// This is a sample custom implemented Navigation. It combines a MasterDetail and a TabbedPage.
+    /// </summary>
+    public class CustomImplementedNav : Xamarin.Forms.MasterDetailPage, IFreshNavigationService
+    {
+        private FreshTabbedNavigationContainer _tabbedNavigationPage;
+        private Page _contactsPage, _quotesPage;
 
-		public CustomImplementedNav ()
-		{	
-			SetupTabbedPage ();
-			CreateMenuPage ("Menu");
-			RegisterNavigation ();
-		}
+        public CustomImplementedNav()
+        {
+            SetupTabbedPage();
+            CreateMenuPage("Menu");
+            RegisterNavigation();
+        }
 
-		void SetupTabbedPage()
-		{
-			_tabbedNavigationPage = new FreshTabbedNavigationContainer ();
-			_contactsPage = _tabbedNavigationPage.AddTab<ContactListPageModel> ("Contacts", "contacts.png");
-			_quotesPage = _tabbedNavigationPage.AddTab<QuoteListPageModel> ("Quotes", "document.png");
-			this.Detail = _tabbedNavigationPage;
-		}
+        private void SetupTabbedPage()
+        {
+            _tabbedNavigationPage = new FreshTabbedNavigationContainer();
+            _contactsPage = _tabbedNavigationPage.AddTab<ContactListPageModel>("Contacts", "contacts.png");
+            _quotesPage = _tabbedNavigationPage.AddTab<QuoteListPageModel>("Quotes", "document.png");
+            this.Detail = _tabbedNavigationPage;
+        }
 
-		protected void RegisterNavigation()
-		{
-			FreshIOC.Container.Register<IFreshNavigationService> (this);
-		}
+        protected void RegisterNavigation()
+        {
+            FreshIOC.Container.Register<IFreshNavigationService>(this);
+        }
 
-		protected void CreateMenuPage(string menuPageTitle)
-		{
-			var _menuPage = new ContentPage ();
-			_menuPage.Title = menuPageTitle;
-			var listView = new ListView();
+        protected void CreateMenuPage(string menuPageTitle)
+        {
+            var _menuPage = new ContentPage();
+            _menuPage.Title = menuPageTitle;
+            var listView = new ListView();
 
-			listView.ItemsSource = new string[] { "Contacts", "Quotes", "Modal Demo" };
+            listView.ItemsSource = new string[] { "Contacts", "Quotes", "Modal Demo" };
 
-			listView.ItemSelected += async (sender, args) =>
-			{
+            listView.ItemSelected += async (sender, args) =>
+            {
+                switch ((string)args.SelectedItem)
+                {
+                    case "Contacts":
+                        _tabbedNavigationPage.CurrentPage = _contactsPage;
+                        break;
 
-				switch ((string)args.SelectedItem) {
-				case "Contacts":
-					_tabbedNavigationPage.CurrentPage = _contactsPage;
-					break;
-				case "Quotes":
-					_tabbedNavigationPage.CurrentPage = _quotesPage;
-					break;
-				case "Modal Demo":
-                    var modalPage = FreshPageModelResolver.ResolvePageModel<ModalPageModel>();
-					await PushPage(modalPage, null, true);
-					break;
-				default:
-				break;
-				}
+                    case "Quotes":
+                        _tabbedNavigationPage.CurrentPage = _quotesPage;
+                        break;
 
-				IsPresented = false;
-			};
+                    case "Modal Demo":
+                        var modalPage = FreshPageModelResolver.ResolvePageModel<ModalPageModel>();
+                        await PushPage(modalPage, null, true);
+                        break;
 
-			_menuPage.Content = listView;
+                    default:
+                        break;
+                }
 
-			Master = new NavigationPage(_menuPage) { Title = "Menu" };
-		}
+                IsPresented = false;
+            };
 
-        public virtual async Task PushPage (Xamarin.Forms.Page page, FreshBasePageModel model, bool modal = false, bool animated = true)
-		{
-			if (modal)
-                await Navigation.PushModalAsync (new NavigationPage(page), animated);
-			else
-                await ((NavigationPage)_tabbedNavigationPage.CurrentPage).PushAsync (page, animated); 
-		}
+            _menuPage.Content = listView;
 
-        public virtual async Task PopPage (bool modal = false, bool animate = true)
-		{
-			if (modal)
-				await Navigation.PopModalAsync ();
-			else
-				await ((NavigationPage)_tabbedNavigationPage.CurrentPage).PopAsync (); 
-		}
-	}
+            Master = new NavigationPage(_menuPage) { Title = "Menu" };
+        }
+
+        public async Task PopToRoot(bool animate = true)
+        {
+            await (Detail as NavigationPage).PopToRootAsync(animate);
+        }
+
+        public virtual async Task PushPage(Xamarin.Forms.Page page, FreshBasePageModel model, bool modal = false, bool animated = true)
+        {
+            if (modal)
+                await Navigation.PushModalAsync(new NavigationPage(page), animated);
+            else
+                await ((NavigationPage)_tabbedNavigationPage.CurrentPage).PushAsync(page, animated);
+        }
+
+        public virtual async Task PopPage(bool modal = false, bool animate = true)
+        {
+            if (modal)
+                await Navigation.PopModalAsync();
+            else
+                await ((NavigationPage)_tabbedNavigationPage.CurrentPage).PopAsync();
+        }
+    }
 }
-
