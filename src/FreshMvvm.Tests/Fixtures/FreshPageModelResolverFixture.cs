7622e01cc6c60fc51e5af92a3853fbbca956c372
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2015-12-10 15:01:44 +0300

    Added tests for FreshPageModelResolver

--- src/FreshMvvm.Tests/Fixtures/FreshPageModelResolverFixture.cs
+++ src/FreshMvvm.Tests/Fixtures/FreshPageModelResolverFixture.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using FreshMvvm.Tests.Mocks;
+using NUnit.Framework;
 
 namespace FreshMvvm.Tests.Fixtures
 {
@@ -6,9 +8,35 @@ namespace FreshMvvm.Tests.Fixtures
 	public class FreshPageModelResolverFixture
 	{
 		[TestCase]
-		public void ResolvePageModelTest()
+		public void Test_ResolvePageModel_Not_Found()
 		{
-			
+			Assert.Throws<Exception>(() =>
+			{
+				FreshPageModelResolver.ResolvePageModel<MockFreshBasePageModel>();
+			});
+		}
+
+		[TestCase]
+		public void Test_ResolvePageModel()
+		{
+			var page = FreshPageModelResolver.ResolvePageModel<MockContentPageModel>();
+			var context = page.BindingContext as MockContentPageModel;
+
+			Assert.IsNotNull(context);
+			Assert.IsNotNull(context.CurrentPage);
+			Assert.IsNotNull(context.CoreMethods);
+		}
+
+		[TestCase("test data")]
+		public void Test_ResolvePageModel_With_Init(object data)
+		{
+			var page = FreshPageModelResolver.ResolvePageModel<MockContentPageModel>(data);
+			var context = page.BindingContext as MockContentPageModel;
+
+			Assert.IsNotNull(context);
+			Assert.IsNotNull(context.CurrentPage);
+			Assert.IsNotNull(context.CoreMethods);
+			Assert.AreSame(data, context.Data);
 		}
 	}
 }
