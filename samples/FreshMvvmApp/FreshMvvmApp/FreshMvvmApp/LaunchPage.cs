5213a7032d1c00e258e5c20d8977040ffdb19c1c
Pierce Boggan <piboggan@microsoft.com>
2018-04-24 00:21:02 -0400

    Update sample for FreshMvvm.

---
+++ samples/FreshMvvmApp/FreshMvvmApp/FreshMvvmApp/LaunchPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace FreshMvvmApp
+{
+    public class LaunchPage : ContentPage
+    {
+        public LaunchPage (App app)
+        {
+            Title = "Select Sample";
+            var list = new ListView ();
+            list.ItemsSource = new List<string> {
+                "Basic Navigation",
+                "Master Detail",
+                "Tabbed Navigation",
+                "Custom Navigation",
+                "Tabbed (FO) Navigation",
+                "Multiple Navigation"
+            };
+            list.ItemTapped += (object sender, ItemTappedEventArgs e) => {
+                if ((string)e.Item == "Basic Navigation")
+                    app.LoadBasicNav ();
+                else if ((string)e.Item == "Master Detail")
+                    app.LoadMasterDetail ();
+                else if ((string)e.Item == "Tabbed Navigation")
+                    app.LoadTabbedNav ();
+                else if ((string)e.Item == "Tabbed (FO) Navigation")
+                    app.LoadFOTabbedNav ();
+                else if ((string)e.Item == "Custom Navigation")
+                    app.LoadCustomNav ();
+                else if ((string)e.Item == "Multiple Navigation")
+                    app.LoadMultipleNavigation ();
+            };
+            this.Content = list;
+        }
+    }
+}
+
---
+++ samples/FreshMvvmApp/FreshMvvmApp/FreshMvvmApp/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PropertyChanged;
+
+namespace FreshMvvmApp
+{
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class Contact
+    {
+        public Contact ()
+        {
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
+
