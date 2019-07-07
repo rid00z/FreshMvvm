eae1395821b54539048f16e122707347e67efffb
tebeco <tebeco@gmail.com>
2019-07-07 15:54:13 +0200

    duplicating existing file test

---
+++ test/FreshMvvm.Tests/PageModelCoreMethodsFixture.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using FreshMvvm.Tests.Mocks;
+using NSubstitute;
+using NUnit.Framework;
+using Xamarin.Forms;
+
+namespace FreshMvvm.Tests.Fixtures
+{
+	[TestFixture]
+	class PageModelCoreMethodsFixture
+	{
+	    PageModelCoreMethods _coreMethods;
+	    IFreshNavigationService _navigationMock;
+	    Page _page;
+	    FreshBasePageModel _pageModel;
+
+        void SetupFirstNavigationAndPage()
+	    {
+	        _navigationMock = Substitute.For<IFreshNavigationService>();
+	        FreshIoc.Container.Register<IFreshNavigationService>(_navigationMock, "firstNav");
+
+	        _page = FreshPageModelResolver.ResolvePageModel<MockContentPageModel>();
+	        _pageModel = _page.BindingContext as MockContentPageModel;
+	        _pageModel.CurrentNavigationServiceName = "firstNav";
+
+
+	        _coreMethods = new PageModelCoreMethods(_page, _pageModel);
+        }
+
+        [Test]
+	    public async Task model_property_populated_by_action()
+	    {
+            SetupFirstNavigationAndPage();
+
+	        const string item = "asj";
+	        await _coreMethods.PushPageModel<MockItemPageModel>(pm => pm.Item = item);
+
+	        _navigationMock.Received().PushPage(Arg.Any<Page>(), Arg.Is<MockItemPageModel>(o => o.Item == item), Arg.Any<bool>(), Arg.Any<bool>());
+        }
+    }
+}
